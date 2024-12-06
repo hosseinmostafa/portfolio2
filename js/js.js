b46a975a863ed9c0241361301094438c9e92fe5f
hosseinmostafa <hesseinnmostafaa@gmail.com>
2024-12-06 21:14:02 +0200

    update

--- js/js.js
+++ js/js.js
@@ -60,3 +60,23 @@ window.addEventListener('scroll', () => {
         }
     })
 });
+
+// scroll
+
+ScrollReveal({
+    // reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+});
+
+ScrollReveal().reveal('.home .info h2,.home .info h1, .section-title-01, .section-title-02', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('.home .info .btn', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.home-img, .about-img', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal(' .contact-right', { delay: 600, origin: 'right' });
+ScrollReveal().reveal(' .content', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.about .professional-list li', { delay: 500, origin: 'right', interval: 200 });
+ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2', { delay: 700, origin: 'left' });
+ScrollReveal().reveal('.experience-card, .service-card, .education, .profolio .img-card', { delay: 800, origin: 'left', interval: 200 });
